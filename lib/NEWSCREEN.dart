22216f445d5f7fed41f6d96a6b123d18238cff05
Srimadhaven Thirumurthy <72145799+MaddyDev-glitch@users.noreply.github.com>
2022-03-01 14:59:57 -0500

    Update NEWSCREEN.dart

--- lib/NEWSCREEN.dart
+++ lib/NEWSCREEN.dart
@@ -1,128 +1,128 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
-// import 'package:flutter/material.dart';
-// import 'package:vector_math/vector_math_64.dart' as vector;
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
-// ArCoreController arCoreController = ArCoreController();
+ArCoreController arCoreController = ArCoreController();
 
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   runApp(MyApp());
-// }
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 
-// class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
 
-//   // This widget is the root of your application.
-//   ArCoreController arCoreController = ArCoreController();
+  // This widget is the root of your application.
+  ArCoreController arCoreController = ArCoreController();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     void onTapHandler(String name) {
-//       showDialog<void>(
-//         context: context,
-//         builder: (BuildContext context) =>
-//             AlertDialog(content: Text('onNodeTap on $name')),
-//       );
-//     }
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: MyHomePage(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    void onTapHandler(String name) {
+      showDialog<void>(
+        context: context,
+        builder: (BuildContext context) =>
+            AlertDialog(content: Text('onNodeTap on $name')),
+      );
+    }
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
 
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
 
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Hello World'),
-//         ),
-//         body: ArCoreView(
-//           enableTapRecognizer: true,
-//           type: ArCoreViewType.STANDARDVIEW,
-//           onArCoreViewCreated: _onArCoreViewCreated,
-//           // ArCoreViewType= ArCoreViewType.STANDARDVIEW,
-//         ),
-//       ),
-//     );
-//   }
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World'),
+        ),
+        body: ArCoreView(
+          enableTapRecognizer: true,
+          type: ArCoreViewType.STANDARDVIEW,
+          onArCoreViewCreated: _onArCoreViewCreated,
+          // ArCoreViewType= ArCoreViewType.STANDARDVIEW,
+        ),
+      ),
+    );
+  }
 
-//   void _onArCoreViewCreated(ArCoreController controller) {
-//     arCoreController = controller;
+  void _onArCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
 
-//     _addSphere(arCoreController);
-//     _addCylinder(arCoreController);
-//     _addCube(arCoreController);
-//     arCoreController.onNodeTap = (name) => onTapHandler(name);
-//     arCoreController.onPlaneTap = _onPlaneTapHandler;
-//   }
+    _addSphere(arCoreController);
+    _addCylinder(arCoreController);
+    _addCube(arCoreController);
+    arCoreController.onNodeTap = (name) => onTapHandler(name);
+    arCoreController.onPlaneTap = _onPlaneTapHandler;
+  }
 
-//   void _addSphere(ArCoreController controller) {
-//     final material = ArCoreMaterial(
-//       color: Color.fromARGB(120, 66, 134, 244),
-//     );
-//     final sphere = ArCoreSphere(
-//       materials: [material],
-//       radius: 0.1,
-//     );
-//     final node = ArCoreNode(
-//       shape: sphere,
-//       position: vector.Vector3(0, 0, -1.5),
-//     );
-//     controller.addArCoreNode(node);
-//   }
+  void _addSphere(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Color.fromARGB(120, 66, 134, 244),
+    );
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(0, 0, -1.5),
+    );
+    controller.addArCoreNode(node);
+  }
 
-//   void _addCylinder(ArCoreController controller) {
-//     final material = ArCoreMaterial(
-//       color: Colors.red,
-//       reflectance: 1.0,
-//     );
-//     final cylinder = ArCoreCylinder(
-//       materials: [material],
-//       radius: 0.5,
-//       height: 0.3,
-//     );
-//     final node = ArCoreNode(
-//       shape: cylinder,
-//       position: vector.Vector3(0.0, -0.5, -2.0),
-//     );
-//     controller.addArCoreNode(node);
-//   }
+  void _addCylinder(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Colors.red,
+      reflectance: 1.0,
+    );
+    final cylinder = ArCoreCylinder(
+      materials: [material],
+      radius: 0.5,
+      height: 0.3,
+    );
+    final node = ArCoreNode(
+      shape: cylinder,
+      position: vector.Vector3(0.0, -0.5, -2.0),
+    );
+    controller.addArCoreNode(node);
+  }
 
-//   void _addCube(ArCoreController controller) {
-//     final material = ArCoreMaterial(
-//       color: Color.fromARGB(120, 66, 134, 244),
-//       metallic: 1.0,
-//     );
-//     final cube = ArCoreCube(
-//       materials: [material],
-//       size: vector.Vector3(0.5, 0.5, 0.5),
-//     );
-//     final node = ArCoreNode(
-//       shape: cube,
-//       position: vector.Vector3(-0.5, 0.5, -3.5),
-//     );
+  void _addCube(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Color.fromARGB(120, 66, 134, 244),
+      metallic: 1.0,
+    );
+    final cube = ArCoreCube(
+      materials: [material],
+      size: vector.Vector3(0.5, 0.5, 0.5),
+    );
+    final node = ArCoreNode(
+      shape: cube,
+      position: vector.Vector3(-0.5, 0.5, -3.5),
+    );
 
-//     controller.addArCoreNode(node);
-//   }
+    controller.addArCoreNode(node);
+  }
 
-//   @override
-//   void dispose() {
-//     arCoreController.dispose();
-//     super.dispose();
-//   }
-// }
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
+}
